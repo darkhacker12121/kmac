50e16f9435e07e1a9d2639952bd17813a97913dd
darkhacker12121 <71131168+darkhacker12121@users.noreply.github.com>
2020-09-11 22:59:54 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,15 @@
+from tobrot.sample_config import Config
+#Fill your all data, read readme for reference
+
+class Config(Config):
+	TG_BOT_TOKEN= "11395112075:AAEbYxHJ3O_ds2uSOrkt_Mjn8f3s3qJPwHY"
+	APP_ID = 1383845
+	API_HASH = "0e3d2c299cc3c5cc26c283cecd2eb97c"
+	OWNER_ID = "1131653685" 
+	AUTH_CHANNEL = [-407986437]
+	DESTINATION_FOLDER = "Gdrive" #Name of your folder read readme
+	RCLONE_CONFIG = "type = drive
+scope = drive
+root_folder_id = 1VWc2dQUreU6wmf53LVHc5uXe4HdiOWIe
+token = {"access_token":"ya29.a0AfH6SMDHXDSPoKnQ46EgHkcPkXsp4fadse1FAE679xm-8fLMQaDcWp58BdioIuYf-jXd8BiamzpqVSZtosxriRwjiIk12Hyb7_yU6dPeSvHl1X10mh2QLucBL52x-wY2FXRJP8DbAl5q0_EIMf0fRPDLxz1rpyVuM8U","token_type":"Bearer","refresh_token":"1//0g_Wkm9UhcgakCgYIARAAGBASNwF-L9Ir_KOxT2Xj9BsGOL53N2xSHs2e0KCcdb6YtxiyCJunzPqlDRm3Y2eHsLdm4DT-TrkwDVg","expiry":"2020-09-11T20:53:24.237937434+05:30"}
+"
